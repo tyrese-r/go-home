3ab74f0af7322ada72fea2425ac913ef9b0da24b
Stefanos Michaelides <stefanos2291@gmail.com>
2025-03-09 22:24:03 +0700

    Close func

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -19,8 +19,11 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to connect to database: %v", err)
 	}
-	defer db.Close()
-
+	defer func() {
+		if err := db.Close(); err != nil {
+			log.Printf("Error closing database: %v", err)
+		}
+	}()
 	// Initialize repositories
 	deviceRepo := repository.NewDeviceRepository(db)
 
